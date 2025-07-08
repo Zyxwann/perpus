1836c2fadedcf309f720a48707e3dc3f55df2f8e
Exxonnnn <fadliilmipratama1317@gmail.com>
2025-07-08 13:58:46 +0800

    Revert "feat: module Member"

--- Modules/Member/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-member::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('member.name') !!}</p>
-</x-member::layouts.master>
--- Modules/Member/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Member\Http\Controllers\MemberController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('members', MemberController::class)->names('member');
-});
